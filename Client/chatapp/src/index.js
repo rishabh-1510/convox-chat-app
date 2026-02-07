147a28b75c1712801859d06a74e8692fd799681a
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-02-07 19:23:42 +0530

    Frontent 2

--- Client/chatapp/src/index.js
+++ Client/chatapp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Provider, provider} from "react-redux"
+import {Provider} from "react-redux"
 import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
