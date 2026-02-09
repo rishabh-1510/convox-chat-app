d5409607546b237e688208bc310909378c45b5d6
rishabh-1510 <158290586+rishabh-1510@users.noreply.github.com>
2026-02-09 23:38:25 +0530

    Frontend little issues

--- Client/chatapp/src/App.js
+++ Client/chatapp/src/App.js
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-red-400 flex items-center justify-center">
       <h1 className="text-5xl font-extrabold text-white">
-        Tailwind FINALLY WORKS 🚀
+        Hello
       </h1>
     </div>
   );
